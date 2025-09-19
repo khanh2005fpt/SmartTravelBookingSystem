8cd543ba615e13dbfeda5f991e52ed0b156677dd
ZUUU\PC <vuhao2k4hht@gmail.com>
2025-09-19 11:26:51 +0700

    them profile controller

--- src/java/controller/account/Hello.java
+++ src/java/controller/account/Hello.java
@@ -41,7 +41,7 @@ public class Hello extends HttpServlet {
             out.println("</html>");
         }
     } 
-//hello
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
      * Handles the HTTP <code>GET</code> method.
